e4fb7afd6a2772145fea36c4189e7f3d947d763c
andrewechang <changandrew240@gmail.com>
2025-01-30 15:41:08 -0800

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//This is the first lab (Lab 1 - setup)
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    printf("Hello, World again!\n");
+    return 0;
+}
